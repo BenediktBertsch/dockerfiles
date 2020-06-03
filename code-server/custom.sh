bc29c74e8d66fb51174967a9a4dcd561b2bd68bb
Benedikt Bertsch <benedikt.bertsch@gmail.com>
2020-06-03 10:59:20 +0200

    fix

--- code-server/custom.sh
+++ code-server/custom.sh
@@ -1,20 +1,33 @@
 #!/bin/bash
-
 # ENV
-config='/config'
-workspace='/workspace'
 port=$PORT
 
 # IP
 ipv4=$(/sbin/ip -o -4 addr list eth0 | awk '{print $4}' | cut -d/ -f1)
 
-# Check if dependencies script exists
-if [ -f /dependencies/dependencies.sh]
+# Check folders
+if [ ! -d /data/dependencies ]
     then 
-        chmod 777 /dependencies/dependencies.sh && /dependencies/dependencies.sh
+        mkdir /data/dependencies && chmod -R 777 /data/dependencies
+fi
+
+if [ ! -d /data/config ]
+    then 
+        mkdir /data/config && chmod -R 777 /data/config
+fi
+
+if [ ! -d /data/workspace ]
+    then 
+        mkdir /data/workspace && chmod -R 777 /data/workspace
+fi
+
+# Check if dependencies script exists
+if [ -f /data/dependencies/dependencies.sh]
+    then 
+        chmod 777 /data/dependencies/dependencies.sh && /data/dependencies/dependencies.sh
     else 
-        echo "Customfile does not exist" 
+        echo "Customfile does not exist, generating..." && touch /data/dependencies/dependencies.sh && echo "#!/bin/bash" >> /data/dependencies/dependencies.sh
 fi
 
 # start Code-Server
-code-server --bind-addr $ipv4:$port --config $config --user-data-dir $workspace
+code-server --bind-addr $ipv4:$port --config /data/config --user-data-dir /data/workspace
