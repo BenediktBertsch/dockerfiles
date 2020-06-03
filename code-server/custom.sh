2ba44743308e335fc348fa584ffc9949d76332ec
Benedikt Bertsch <benedikt.bertsch@gmail.com>
2020-06-03 11:56:23 +0200

    fix

--- code-server/custom.sh
+++ code-server/custom.sh
@@ -10,9 +10,6 @@ else
   echo "starting with no password"
 fi
 
-# IP
-ipv4=$(/sbin/ip -o -4 addr list eth0 | awk '{print $4}' | cut -d/ -f1)
-
 # Check folders
 mkdir -p /data/{extensions,data,workspace,dependencies}
 
@@ -21,7 +18,7 @@ if [ -f /data/dependencies/dependencies.sh ]
     then 
         chmod 777 /data/dependencies/dependencies.sh && /data/dependencies/dependencies.sh
     else 
-        echo "Customfile does not exist, generating..." && touch /data/dependencies/dependencies.sh && echo "#!/bin/bash" >> /data/dependencies/dependencies.sh
+        echo "Customfile does not exist, generating..." && touch /data/dependencies/dependencies.sh && echo "#!/bin/bash" >> /data/dependencies/dependencies.sh && chmod 777 /data/dependencies/dependencies.sh
 fi
 
 # start Code-Server
