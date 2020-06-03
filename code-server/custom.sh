f49c62dc06da06dda28a90c8de329e9d68c268f8
Benedikt Bertsch <benedikt.bertsch@gmail.com>
2020-06-03 09:54:31 +0200

    fix

--- code-server/custom.sh
+++ code-server/custom.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
+
+# ENV
+config='/config'
+workspace='/workspace'
+
+# IP
+ipv4=$(/sbin/ip -o -4 addr list eth0 | awk '{print $4}' | cut -d/ -f1)
+
+# Check if dependencies script exists
 if [ -f /dependencies/dependencies.sh]
     then 
         /dependencies/dependencies.sh 
     else 
         echo "Customfile does not exist" 
 fi
+
+# start Code-Server
+./code-server --bind-addr $ipv4 --config $config --user-data-dir $workspace
